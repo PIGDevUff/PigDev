3cd097b4901fae2edd16ab7889dde6075253792d
PigDevUFF <pigproj@gmail.com>
2022-07-04 12:20:47 -0300

    Update CPIGLabel.h

--- include/CPIGLabel.h
+++ include/CPIGLabel.h
@@ -6,87 +6,88 @@
 class CPIGLabel:public CPIGSprite{
 
 private:
-string frase;
-int idFonte;
-PIGCor corFonte;
 
-void AtualizaTextura()override{
-    PIGFonte fonte = CPIGGerenciadorFontes::GetFonte(idFonte);
-    larg = fonte->GetLarguraPixelsString(frase);
-    alt = fonte->GetFonteAscent()+fonte->GetFonteDescent()+5;
+    string frase;
+    int idFonte;
+    PIGCor corFonte;
 
-    if (this_thread::get_id()!=PIG_MAIN_THREAD_ID){
-        //printf("atualizando textura <%s> com thread %d (%d)\n",frase.c_str(),this_thread::get_id(),PIG_MAIN_THREAD_ID);
-        return;
+    void AtualizaTextura()override{
+        PIGFonte fonte = CPIGGerenciadorFontes::GetFonte(idFonte);
+        larg = fonte->GetLarguraPixelsString(frase);
+        alt = fonte->GetFonteAscent()+fonte->GetFonteDescent()+5;
+
+        if (this_thread::get_id()!=PIG_MAIN_THREAD_ID){
+            //printf("atualizando textura <%s> com thread %d (%d)\n",frase.c_str(),this_thread::get_id(),PIG_MAIN_THREAD_ID);
+            return;
+        }
+
+        if (text) SDL_DestroyTexture(text);
+        text = SDL_CreateTexture(renderer,SDL_PIXELFORMAT_RGBA8888,SDL_TEXTUREACCESS_TARGET,larg,alt);
+
+        if (SDL_SetRenderTarget(renderer,text)==0){//mudou o target sem erro (==0)
+            SDL_SetRenderDrawColor(renderer,0,0,0,0);
+            SDL_RenderClear(renderer);
+            SDL_SetRenderTarget(renderer,NULL);
+        }else{
+            SDL_DestroyTexture(text);
+            text = NULL;
+        }
+
+        if (text){
+            fonte->EscreveTextura(frase,0,0,text,corFonte);
+            precisaAtualizar = false;
+        }
+
+        SetDimensoes(alt,larg);
+        DefineFrame(0,{0,0,larg,alt});
+        MudaFrameAtual(0);
     }
-
-    if (text) SDL_DestroyTexture(text);
-    text = SDL_CreateTexture(renderer,SDL_PIXELFORMAT_RGBA8888,SDL_TEXTUREACCESS_TARGET,larg,alt);
-
-    if (SDL_SetRenderTarget(renderer,text)==0){//mudou o target sem erro (==0)
-        SDL_SetRenderDrawColor(renderer,0,0,0,0);
-        SDL_RenderClear(renderer);
-        SDL_SetRenderTarget(renderer,NULL);
-    }else{
-        SDL_DestroyTexture(text);
-        text = NULL;
-    }
-
-    if (text){
-        fonte->Escreve(frase,text,corFonte);
-        precisaAtualizar = false;
-    }
-
-    SetDimensoes(alt,larg);
-    DefineFrame(0,{0,0,larg,alt});
-    MudaFrameAtual(0);
-}
 
 public:
 
-CPIGLabel(string texto, PIGCor cor, int numFonte, int idJanela=0):CPIGSprite(idJanela){
-    idFonte = numFonte;
-    text = NULL;
-    corFonte = cor;
-    frase = texto;
-    precisaAtualizar = true;
-    AtualizaTextura();
-}
+    CPIGLabel(string texto, PIGCor cor, int numFonte, int idJanela=0):CPIGSprite(idJanela){
+        idFonte = numFonte;
+        text = NULL;
+        corFonte = cor;
+        frase = texto;
+        precisaAtualizar = true;
+        AtualizaTextura();
+    }
 
-inline void SetTexto(string texto){
-    frase = texto;
-    AtualizaTextura();
-}
+    inline void SetTexto(string texto){
+        frase = texto;
+        AtualizaTextura();
+    }
 
-inline void SetFonte(int numFonte){
-    idFonte = numFonte;
-    AtualizaTextura();
-}
+    inline void SetFonte(int numFonte){
+        idFonte = numFonte;
+        AtualizaTextura();
+    }
 
-inline void SetCorFonte(PIGCor novaCor){
-    corFonte = novaCor;
-    AtualizaTextura();
-}
+    inline void SetCorFonte(PIGCor novaCor){
+        corFonte = novaCor;
+        AtualizaTextura();
+    }
 
-inline int GetAltura(){
-    return alt;
-}
+    inline int GetAltura(){
+        return alt;
+    }
 
-inline int GetLargura(){
-    return larg;
-}
+    inline int GetLargura(){
+        return larg;
+    }
 
-inline string GetTexto(){
-    return frase;
-}
+    inline string GetTexto(){
+        return frase;
+    }
 
-inline int GetFonte(){
-    return idFonte;
-}
+    inline int GetFonte(){
+        return idFonte;
+    }
 
-inline PIGCor GetCorFonte(){
-    return corFonte;
-}
+    inline PIGCor GetCorFonte(){
+        return corFonte;
+    }
 
 };
 
